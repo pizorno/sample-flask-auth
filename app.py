36509f71b22e1c15c73093a8181bf5e0283059e5
Eduardo Pizorno <pizorno@gmail.com>
2024-10-27 11:08:15 -0300

    feat: Estruturando conexão ao banco com SQLAlchemy

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "your_secret_key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite"
+db = SQLAlchemy(app)
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
