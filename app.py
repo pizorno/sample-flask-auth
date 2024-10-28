58a4d13dfcdc549cf27c66a5ec88fdda57f4ce62
Eduardo Pizorno <pizorno@gmail.com>
2024-10-28 10:16:42 -0300

    feat: Adicionando flask-login e rota login

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models.user import User
 from database import db
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite"
 
+login_manager = LoginManager()
+
 db.init_app(app)
+login_manager.init_app(app)
+
+@app.route("/login", methods=["POST"])
+def login():
+    data.request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        pass
+
+    return jsonify({"message": "Credenciais inválidas"}), 400
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
